d457e6b530a9f458da381bf07a1a4f6419a9b136
Chris Hart <chrstphrhrt@gmail.com>
2014-07-09 17:57:34 -0400

    specify python version for heroku

--- hello.py
+++ hello.py
@@ -1,8 +1,22 @@
 import os
 from flask import Flask
+from flask.ext.admin import Admin
+from flask.ext.admin.contrib.sqla import ModelView
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
 
 @app.route('/')
 def hello():
 	return 'Hello world!'
+	
+class Post(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	title = db.Column(db.Unicode(120))
+	text = db.Column(db.UnicodeText, nullable=False)
+
+admin = Admin(app)
+admin.add_view(ModelView(Post, db.session))
+
+app.run()
